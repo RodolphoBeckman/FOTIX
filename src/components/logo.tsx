6f9aaae990758705e12c4d9a9eb9a23bd55d8c9f
Rodolpho Beckman <rodolpho.beckman@gmail.com>
2025-07-28 19:02:29 +0000

    simbulo

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -10,19 +10,9 @@ export function Logo(props: React.SVGProps<SVGSVGElement>) {
       xmlns="http://www.w3.org/2000/svg"
       {...props}
     >
-      <rect width="32" height="32" rx="8" fill="hsl(var(--primary))" />
-      <text
-        x="50%"
-        y="50%"
-        dominantBaseline="middle"
-        textAnchor="middle"
-        fontFamily="Playfair Display, serif"
-        fontSize="18"
-        fontWeight="bold"
-        fill="hsl(var(--primary-foreground))"
-      >
-        FX
-      </text>
+        <path d="M22.2857 0H9.71429C4.34921 0 0 4.34921 0 9.71429V22.2857C0 27.6508 4.34921 32 9.71429 32H22.2857C27.6508 32 32 27.6508 32 22.2857V9.71429C32 4.34921 27.6508 0 22.2857 0Z" fill="hsl(var(--primary))"/>
+        <path d="M16.4286 10.5714C13.2 10.5714 10.5714 13.2 10.5714 16.4286V21.4286H16.4286C19.6571 21.4286 22.2857 18.8 22.2857 15.5714V10.5714H16.4286Z" fill="hsl(var(--primary-foreground))" fillOpacity="0.8"/>
+        <path d="M15.5714 21.4286C18.8 21.4286 21.4286 18.8 21.4286 15.5714V10.5714H15.5714C12.3429 10.5714 9.71429 13.2 9.71429 16.4286V21.4286H15.5714Z" fill="hsl(var(--primary-foreground))"/>
     </svg>
   );
 }
