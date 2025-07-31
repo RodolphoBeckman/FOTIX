78b03d123c73c1d51fd1ec2b5725012dd1d21013
Rodolpho Beckman <rodolpho.beckman@gmail.com>
2025-07-31 17:12:31 +0000

    [14:10:36.690] Running build in Washington, D.C., USA (East) – iad1 [14:

--- src/app/api/genkit/[...slug]/route.ts
+++ src/app/api/genkit/[...slug]/route.ts
@@ -1,7 +1,6 @@
-
-import {nextHandler} from '@genkit-ai/next';
+import {createNextApiHandler} from '@genkit-ai/next';
 import '@/ai/flows/generate-product-info';
 
 export const maxDuration = 60; // Set max duration to 60 seconds
 
-export const {GET, POST} = nextHandler();
+export const {GET, POST} = createNextApiHandler();
