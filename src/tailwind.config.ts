228a934b4df38b59f1d7f33d6eb97c40bc5b3eca
Rodolpho Beckman <rodolpho.beckman@gmail.com>
2025-08-02 13:23:31 +0000

    a animação ao abrir o app pode deixar aumentar o tempo pra 3 segundos?

--- src/tailwind.config.ts
+++ src/tailwind.config.ts
@@ -108,7 +108,7 @@ export default {
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
-        'logo-pulse': 'logo-pulse 4s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+        'logo-pulse': 'logo-pulse 3s cubic-bezier(0.4, 0, 0.6, 1) infinite',
       },
     },
   },
